079cf7c71e83eac56acdd2c31ddd32f2bc0b3983
Roy Wiggins <Roy.Wiggins@nyulangone.org>
2021-12-23 20:21:36 +0000

    update __init__.py

--- dcmannotate/__init__.py
+++ dcmannotate/__init__.py
@@ -1,3 +1,14 @@
-from .annotations import Point, Measurement, PointMeasurement, Ellipse, Annotations, AnnotationSet, DicomVolume
+"""An experimental python library for generating simple annotations on DICOM volumes."""
+
+__version__ = "0.0.1"
+from .annotations import (
+    Point,
+    Measurement,
+    PointMeasurement,
+    Ellipse,
+    Annotations,
+    AnnotationSet,
+    DicomVolume,
+)
 from .visage import VisageWriter
 from .writers import SecondaryCaptureWriter, SRWriter
